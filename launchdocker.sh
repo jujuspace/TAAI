dff208134a3e272c412895b97348622937e70dcb
Juseong Park <163384189+jujuspace@users.noreply.github.com>
2024-10-25 19:37:54 +0900

    Update launchdocker.sh

--- launchdocker.sh
+++ launchdocker.sh
@@ -2,7 +2,8 @@ sudo docker run --gpus all -it --net=host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v `pwd`/share:/root/share \
+    -v /media/ju/Angel1/server_data:/root/server_data \
     -w /root/share \
-    --name 'container' \
-    'docker name here eg., jusungnow/graph:1.0.0' \ 
+    --name lane-net \
+    jusungnow/lane-net:1.1.0 \
     bash
